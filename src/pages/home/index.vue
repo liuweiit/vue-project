bb06481d2a99fae9582dded7366b8b1a97a2b121
刘伟-五思 <liuweiit@utourworld.com>
2018-06-04 15:41:08 +0800

    vue-cli init

---
+++ src/pages/home/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>this is Home</div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  methods: {
+    aa: function() {
+      console.log(1)
+    },
+    b: function() {
+      
+    }
+  }
+}
+</script>
